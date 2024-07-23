9f8c71e5273a8d916454f7aa9e7a9c40638ee388
brennaswilson <102930006+brennaswilson@users.noreply.github.com>
2024-07-22 19:23:50 -0600

    plant queries updated w/ better inserts

--- plants_queries.sql
+++ plants_queries.sql
@@ -1,20 +1,44 @@
 
 -- Insert Statements for all tables
 
--- Query for add a new plant  with colon : character being used to 
+-- Query to add a new plant  with colon : character being used to 
 -- denote the variables that will have data from the backend programming language
-INSERT INTO Plants (displayName, isInside, plantedDate) VALUES (:displayName, :isInside)
+INSERT INTO Plants (displayName, isInside, currentLight, plantTypeID, waterInterval, fertilizerInterval, plantedDate) 
+VALUES (:displayName, :isInside_from_dropdown_Input, :currentLight_from_dropdown_Input, :plantType_id_from_dropdown_Input, :waterInterval, :fertilizerInterval, :plantedDate_from_calendar)
 
--- Query for add a new soiltype with colon : character being used to 
+-- Query to add a new plant type  with colon : character being used to 
+-- denote the variables that will have data from the backend programming language
+INSERT INTO Plants (commonName, toxicCat, toxicDog, preferredLight, latinName) 
+VALUES (:commonName, :toxicCat_from_dropdown_Input, :toxicDog_from_dropdown_Input, :preferredLight_from_dropdown_Input, :latinName)
+
+-- Query to add a new soiltype with colon : character being used to 
 -- denote the variables that will have data from the backend programming language
 INSERT INTO SoilTypes (soilType, soilDescription) VALUES (:soilType, :soilDescription)
 
--- Query for add a new wateringevent functionality with colon : character being used to 
+-- Query to add a new wateringevent functionality with colon : character being used to 
 -- denote the variables that will have data from the backend programming language
-INSERT INTO WateringEvents (wateringDate, plantID) VALUES (:wateringDate, :plantID)
+INSERT INTO WateringEvents (wateringDate, plantID) VALUES (:wateringDate_from_calendar, :plant_id_from_dropdown_Input)
 
--- Query for add a new fertilizingevent functionality with colon : character being used to 
+-- Query to add a new fertilizingevent functionality with colon : character being used to 
 -- denote the variables that will have data from the backend programming language
-INSERT INTO FertilizingEvents (fertilizingDate, plantID) VALUES (:fertilizingDate, :plantID)
+INSERT INTO FertilizingEvents (fertilizingDate, plantID) VALUES (:fertilizingDate_from_calendar, :plantID_from_dropdown_Input)
+
+-- associate a plant with a soilType (M-to-M relationship addition)
+INSERT INTO PlantSoils (plantID, soilID) VALUES (:plant_id_from_dropdown_Input, :soil_id_from_dropdown_Input)
+
+
+
+
+-- Select Statements for all tables
+
+SELECT * FROM Plants;
+
+SELECT * FROM PlantTypes;
+
+SELECT * FROM SoilTypes;
+
+SELECT * FROM WateringEvents;
+
+SELECT * FROM FertilizingEvents;
 
 
