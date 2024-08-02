44a88f444acad5aec4f3973e1e1dcf8e1d87ccf3
bkromrey <kromreyb@oregonstate.edu>
2024-08-01 22:36:13 -0500

    fixed vite config for custom port

--- webapp/frontend/vite.config.js
+++ webapp/frontend/vite.config.js
@@ -1,17 +1,21 @@
+// Following configuration adapted from the CS340 starter code.
+// Date Accessed: 1 August 2024
+// URL: https://github.com/osu-cs340-ecampus/react-starter-app
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    loader: "jsx"
+  },
   server: {
-
-    // Following configuration adapted from the CS340 starter code.
-    // Date Accessed: 1 August 2024
-    // URL: https://github.com/osu-cs340-ecampus/react-starter-app
-    
-    // Use VITE_PORT from your .env, or default to a port if not specified
-    port: parseInt(process.env.VITE_PORT, 10) || 5173
+    port: parseInt(process.env.VITE_PORT, 10) || 5173     // Use VITE_PORT from your .env, or default to a port if not specified
   }
 })
 
