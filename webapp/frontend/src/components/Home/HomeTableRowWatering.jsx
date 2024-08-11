7894e8dd6c0259d4425d215ecc2e7f40c48b94ae
bkromrey <146262682+bkromrey@users.noreply.github.com>
2024-08-11 14:56:18 -0700

    overdue watering & fertilizing events now highlighted in red on homepage

--- webapp/frontend/src/components/Home/HomeTableRowWatering.jsx
+++ webapp/frontend/src/components/Home/HomeTableRowWatering.jsx
@@ -2,47 +2,46 @@
 // Date Accessed: 4 August 2024
 // URL: https://github.com/osu-cs340-ecampus/react-starter-app
 
-import axios from "axios";
-
-import { useNavigate } from "react-router-dom";
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import Form from 'react-bootstrap/Form';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Toast from 'react-bootstrap/Toast';
 
 
 /* eslint-disable react/prop-types */
-const HomeTableRowWatering = ({ PlantsDueWatering, fetchPlantsDueWatering }) => {
-  // Hook that allows us to navigate programmatically
-  const navigate = useNavigate();
-  // Redirect to edit SoilType page
+const HomeTableRowWatering = ({ PlantsDueWatering }) => {
+
+  const NextWateringDate = PlantsDueWatering.NextWateringDate.slice(0,10);
+  const todayDate = new Date().toISOString().slice(0,10);
+  const pastDue = NextWateringDate < todayDate;
 
 
   return (
+    <>
+    
+    {/* anything that is overdue, make red via css */}
+    {pastDue && 
+    <tr key={PlantsDueWatering.plantID}>
+      <td class="pastDueRow"> {PlantsDueWatering.displayName}</td>
+
+      {PlantsDueWatering.LastWateredDate && <td class="pastDueRow">{PlantsDueWatering.LastWateredDate.slice(0,10)}</td> }
+      {!PlantsDueWatering.LastWateredDate && <td class="pastDueRow">none</td> }
+
+      {PlantsDueWatering.NextWateringDate && <td class="pastDueRow">{PlantsDueWatering.NextWateringDate.slice(0,10)}</td>}
+      {!PlantsDueWatering.NextWateringDate && <td class="pastDueRow">none</td>}
+    </tr>
+    }
+
+    {/* anything not overdue, but upcoming, do not make red */}
+    {!pastDue && 
     <tr key={PlantsDueWatering.plantID}>
       <td>{PlantsDueWatering.displayName}</td>
 
+      {PlantsDueWatering.LastWateredDate && <td>{PlantsDueWatering.LastWateredDate.slice(0,10)}</td> }
+      {!PlantsDueWatering.LastWateredDate && <td>none</td> }
 
-      {PlantsDueWatering.LastWateredDate &&
-                        <td>{PlantsDueWatering.LastWateredDate.slice(0,10)}</td>
-      }
-
-      {!PlantsDueWatering.LastWateredDate &&
-                        <td>none</td>
-      }
-
-      {PlantsDueWatering.NextWateringDate &&
-                        <td>{PlantsDueWatering.NextWateringDate.slice(0,10)}</td>
-      }
-
-      {!PlantsDueWatering.NextWateringDate &&
-                        <td>none</td>
-      }
-
+      {PlantsDueWatering.NextWateringDate && <td>{PlantsDueWatering.NextWateringDate.slice(0,10)} </td>}
+      {!PlantsDueWatering.NextWateringDate && <td>none</td>}
     </tr>
+    } 
+
+    </>
   );
 };
 
